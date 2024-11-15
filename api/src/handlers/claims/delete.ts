9ad5893bd442a0438edc50a9ceb1d71298ac4a48
yinkeet <wongyinkeet@gmail.com>
2024-11-15 16:04:51 +0800

    Added remove claim doc endpoint

---
+++ api/src/handlers/claims/delete.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import { pool } from '../../common/db';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import fs from 'fs';
+
+export const deleteClaimDoc = async (req: Request, res: Response) => {
+    const { claimId, docId } = req.params;
+
+    try {
+        const query = `
+        SELECT 
+            path
+        FROM claim_document
+        WHERE claim_id = ? and id = ?
+        `
+        const [rows] = await pool.query<RowDataPacket[]>(query, [claimId, docId]);
+
+        if (rows.length == 0) {
+            res.status(404).json({
+                message: 'Document not found'
+            });
+            return;
+        }
+
+        const filepath = rows[0].path
+        if (!fs.existsSync(filepath)) {
+            res.status(404).json({ message: 'File not found' });
+            return;
+        }
+
+        fs.unlinkSync(filepath);
+
+        await pool.execute(`DELETE FROM claim_document WHERE claim_id = ? and id = ?`, [claimId, docId]);
+
+        res.status(200).json({ message: 'File deleted successfully' });
+    } catch (error: any) {
+        console.log(error)
+        res.status(500).json({ message: 'Failed to delete claim document', error: error });
+    }
+};
--- api/src/handlers/claims/index.ts
+++ api/src/handlers/claims/index.ts
@@ -3,7 +3,8 @@ import { getClaims, getClaimDocs } from './list';
 import { createClaim } from './create';
 import { validateCreateClaim } from './validators';
 import { upload, createClaimDocument } from './upload';
-import { getClaimDocFile } from './details'
+import { getClaimDocFile } from './details';
+import { deleteClaimDoc } from './delete';
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.post('/', validateCreateClaim, createClaim);
 router.post('/:claimId/docs', upload.single('file'), createClaimDocument);
 router.get('/:claimId/docs', getClaimDocs);
 router.get('/:claimId/docs/:docId', getClaimDocFile);
+router.delete('/:claimId/docs/:docId', deleteClaimDoc);
 
 export default router;
